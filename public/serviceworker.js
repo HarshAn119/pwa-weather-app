02592e163e8f3388ee70557960b5d41d2a723a54
Harsh <harshanand119@gmail.com>
2022-12-29 19:14:43 +0530

    UI for weather app

---
+++ public/serviceworker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['index.html', 'offline.html'];
+
+// Install service worker
+self.addEventListener('install', (event) => {});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {});
+
+// Activate the service worker
+self.addEventListener('activate', (event) => {});
